5bd900c880d45152d822bd7bd4c6d8d0ab8b12b3
Disha Gupta <gupta.disha.d26@gmail.com>
2025-01-30 11:15:16 +0530

    creating the calculator file

--- Calculator.java
+++ Calculator.java
@@ -3,25 +3,31 @@ import java.util.List;
 
 public class Calculator {
 
+    // adding 2 numbers
     public int add(int[] numbers) {
         return numbers[0] + numbers[1];
     }
 
+    // subtracting second number from the first
     public int subtract(int[] numbers) {
         return numbers[0] - numbers[1];
     }
 
+    // multiplying the 2 numbers
     public int multiply(int[] numbers) {
         return numbers[0] * numbers[1];
     }
 
+    // dividing the first number by the second
     public double divide(int[] numbers) {
         if (numbers[1] == 0) {
+            // throwing an exception if the second number is 0
             throw new ArithmeticException("Division by zero is not allowed.");
         }
         return (double) numbers[0] / numbers[1];
     }
 
+    // generating fibonacci sequence of n numbers
     public List<Integer> generateFibonacci(int n) {
         List<Integer> fibonacci = new ArrayList<>();
         int a = 0, b = 1;
@@ -34,6 +40,7 @@ public class Calculator {
         return fibonacci;
     }
 
+    // calculating sum of array elements
     public int sumOfArray(int[] array) {
         int sum = 0;
         for (int num : array) {
@@ -42,10 +49,12 @@ public class Calculator {
         return sum;
     }
 
+    // calculating mean of array elements
     public double meanOfArray(int[] array) {
         return (double) sumOfArray(array) / array.length;
     }
 
+    // calulating variance of array elements
     public double varianceOfArray(int[] array) {
         double mean = meanOfArray(array);
         double variance = 0;
@@ -55,6 +64,7 @@ public class Calculator {
         return variance / array.length;
     }
 
+    // calculating standard deviation of array elements
     public double standardDeviationOfArray(int[] array) {
         return Math.sqrt(varianceOfArray(array));
     }
